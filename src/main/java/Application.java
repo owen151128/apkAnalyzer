80a1482bf2fb01d269d31effb06d0a9dacb18362
owen151128 <owen151128@gmail.com>
2019-05-30 13:36:17 +0900

    메인 어플리케이션 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,16 @@
+import util.Console;
+import util.FileUtil;
+import util.InputArgumentsParser;
+import util.ZipUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * @author owen151128@gmail.com
  * <p>
@@ -6,7 +19,89 @@
  * Providing features related to Application class
  */
 public class Application {
+    private static final String APP_NAME = "APK Analyzer v1.0";
+    private static final String LIB = "lib";
+    private static final String MSG_SO_COUNT = "'s So count : ";
+    private static final String MSG_DELETE_WORKING_DIR = "workingDIrectory was deleted!";
+    private static final String MSG_DONE = "APK Analyzer Done!";
+
+    /**
+     * Application's Main method
+     *
+     * @param args Application's Arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Console.bold(APP_NAME);
+
+        InputArgumentsParser argumentsParser = InputArgumentsParser.getInstance(args);  //  initialize Input Arguments parser
+
+        // get Path from Arguments
+        Path workingDirParh = argumentsParser.getWorkingDirectoryPath();
+        Path dirPath = argumentsParser.getInputDirPath();
+        Path[] apkPath = argumentsParser.getInputApkApth();
+
+        // Null check
+        if (workingDirParh == null || (apkPath == null && dirPath == null) || (apkPath != null && dirPath != null)) {
+            if (!argumentsParser.isPrintedUsage()) {
+                InputArgumentsParser.printUsage();
+            }
+
+            return;
+        }
+
+        ArrayList<Path> soPathList = new ArrayList<>();
+
+        if (apkPath != null) {
+            for (Path apk : apkPath) {
+                soPathList.clear();
+                ZipUtil.unZip(apk, workingDirParh); //  unzip apk
+                FileUtil.getAllFilesFromDir(Paths.get(workingDirParh.toAbsolutePath().toString() + File.separator + LIB), soPathList);  //  get So file List
+
+                Console.write(apk.getFileName().toString() + MSG_SO_COUNT + soPathList.size()); //  print So File count
+
+                try {
+                    if (Files.exists(workingDirParh)) {
+                        //  delete unzip apk files
+                        Files.walk(workingDirParh)
+                                .map(Path::toFile)
+                                .sorted(Comparator.reverseOrder())
+                                .forEach(File::delete);
+
+                        System.out.println(MSG_DELETE_WORKING_DIR);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (dirPath != null) {
+            ArrayList<Path> apkList = new ArrayList<>();
+            FileUtil.getAllFilesFromDir(dirPath, apkList);
+
+            for (Path apk : apkList) {
+                soPathList.clear();
+                ZipUtil.unZip(apk, workingDirParh); //  unzip apk
+                FileUtil.getAllFilesFromDir(Paths.get(workingDirParh.toAbsolutePath().toString() + File.separator + LIB), soPathList); // get So file List
+
+                Console.write(apk.getFileName().toString() + MSG_SO_COUNT + soPathList.size()); //  print So File count
+
+                try {
+                    if (Files.exists(workingDirParh)) {
+                        // delete unzip apk files
+                        Files.walk(workingDirParh)
+                                .map(Path::toFile)
+                                .sorted(Comparator.reverseOrder())
+                                .forEach(File::delete);
+
+                        System.out.println(MSG_DELETE_WORKING_DIR);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Console.bold(MSG_DONE);
     }
 }
