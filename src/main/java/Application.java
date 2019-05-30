1506af053aa63fc479df4112165710a0c08a9b01
owen151128 <owen151128@gmail.com>
2019-05-30 10:44:17 +0900

    프로젝트 초기 설정

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * @author owen151128@gmail.com
+ * <p>
+ * Created by owen151128 on 2019-05-30 10:38
+ * <p>
+ * Providing features related to Application class
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
